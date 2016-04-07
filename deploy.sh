8f401430eb918e57248ddcc55b4b2a7c9838defe
BOOLRon <ron.zhang.36@gmail.com>
2016-04-07 09:42:36 +0800

    modify themes

--- deploy.sh
+++ deploy.sh
@@ -8,10 +8,12 @@ git init
 git add .
 git commit -m "update at `date` "
 
+git remote add github git@github.com:BOOLRon/BOOLRon.github.io.git >> /dev/null 2>&1
 echo "### Pushing to Github..."
 git push github master -f
 echo "### Done"
 
+git remote add origin git@git.coding.net:bool_ron/bool_ron.git >> /dev/null 2>&1
 echo "### Pushing to Coding..."
-git push origin coding-pages -f
+git push origin master:coding-pages -f
 echo "### Done"
--- source/_posts/2016-04-06-justfortest.markdown
+++ source/_posts/2016-04-06-justfortest.markdown
@@ -8,4 +8,4 @@ categories:
 
 #### ok, can you c?
 
-![]http://77g05y.com1.z0.glb.clouddn.com/2016-04-06-justfortest/IMG_1711.JPG)
+![](http://77g05y.com1.z0.glb.clouddn.com/2016-04-06-justfortest/IMG_1711.JPG)
